4d0b1f9d39ee16684f1a18178d8035f27a353a82
ashiyana10 <ashiyanahalvadiya123@gmail.com>
2023-03-20 09:01:12 +0530

    Adds the comment

--- src/app/buy-books/buy-books.component.ts
+++ src/app/buy-books/buy-books.component.ts
@@ -11,23 +11,27 @@ export class BuyBooksComponent {
   searchValue!: string;
 
   ngOnInit(): void {
+    // default book sorted by title
     this.getDataBySortField('title');
   }
+
+  /**
+   * perform the sorting as selected category
+   * @param field store the category
+   */
   getDataBySortField(field: string): void {
     // type assertion to tell TypeScript that the propertyToSortBy variable is a keyof the type of the first element of the array.
     let propertyToSortBy = field as keyof typeof bookData[0];
     this.bookData = this.bookData.sort((a, b) =>
       a[propertyToSortBy] < b[propertyToSortBy] ? -1 : 1
     );
-   
   }
 
   /**
    * search the book by title
    * @param value store search value
    */
-  searchBook(value: string) :void{
-   
+  searchBook(value: string) :void{   
     const searchValue = value.toLowerCase();
     let propertyToSortBy = 'title' as keyof typeof bookData[0];
     this.bookData = bookData.filter((item) =>
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,11 +4,16 @@ import { Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
   @Output() searchValue = new EventEmitter<string>();
-  searchBook(search:string){
-    this.searchValue.emit(search)
+
+  /**
+   * pass the search item from child to parent
+   * @param search store the search value
+   */
+  searchBook(search: string): void {
+    this.searchValue.emit(search);
   }
 }
